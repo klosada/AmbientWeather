429d3202674a3cf6c1fc79dd48d17f5b4f3584b9
Katherine Losada <klosada@atlantaregional.org>
2025-07-21 14:31:42 -0400

    Updated active sensors in config, modified database table structure, and fixed timestamp issues.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sensor_api import get_sensor_data
 from database import save_weather_data, init_db
 from logger import logger
+from arcgis_connection import publish_to_arcgis
 
 def main():
     logger.info("Initializing database...")
@@ -12,6 +13,7 @@ def main():
     if not readings.empty:
         logger.info(f"Fetched {len(readings)} readings. Saving to database...")
         save_weather_data(readings)
+        publish_to_arcgis(readings, item_id="6ea917fd50bb413bb25cbc33d34c7119")
         logger.info("Data saved successfully.")
     else:
         logger.warning("No data fetched.")
