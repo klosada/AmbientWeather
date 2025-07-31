3711512138cf0f508ff3b367e6fb0044b0bcd0ab
Katherine Losada <126829073+klosada@users.noreply.github.com>
2025-07-31 16:24:57 -0400

    Disable GIS automation

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from logger import logger
 from sensor_api import get_sensor_data
 from database import save_weather_data, init_db
-from arcgis_connection import publish_to_arcgis
+# from arcgis_connection import publish_to_arcgis
 
 
 def main():
@@ -13,7 +13,7 @@ def main():
     if not readings.empty:
         logger.info(f"Fetched {len(readings)} readings. Saving to database...")
         save_weather_data(readings)
-        publish_to_arcgis(readings, item_id="6ea917fd50bb413bb25cbc33d34c7119")
+#        publish_to_arcgis(readings, item_id="6ea917fd50bb413bb25cbc33d34c7119")
         logger.info("Data saved successfully.")
     else:
         logger.warning("No data fetched.")
